9a763080769c87bc98429d3728dac76e10c7b17a
Dmitrii Sugrobov <dmitrii.sugrobov@leroymerlin.ru>
2019-02-08 18:09:45 +0300

    v0.1

---
+++ ingress-autoswagger.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gobuffalo/packr"
+	"log"
+	"net/http"
+	"os"
+	"text/template"
+)
+
+
+func main() {
+	servicesEnv := os.Getenv("SERVICES")
+	if servicesEnv == "" {
+		log.Println("Environment variable \"SERVICES\" is empty")
+		os.Exit(2)
+	}
+
+	services := createServicesJson(servicesEnv)
+	if services == "null" {
+		log.Println("Incorrect variable \"SERVICES\"")
+		os.Exit(2)
+	}
+
+	log.Println("Server started on 3000 port!")
+	log.Println(services)
+	html, err := packr.NewBox("./templates").FindString("index.html")
+	if err != nil {
+		panic(err)
+	}
+
+	templateEngine, err := template.New("index").Parse(html)
+	if err != nil {
+		panic(err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_ = templateEngine.Execute(w, services)
+	})
+	_ = http.ListenAndServe(":3000", nil)
+}
+
+func createServicesJson(servicesEnv string) string {
+	var services map[string]map[string]interface{}
+	if err := json.Unmarshal([]byte(servicesEnv), &services); err != nil {
+		panic(err)
+	}
+
+	var servicesList []map[string]string
+	for service, params := range services {
+		if params["swagger"] != false {
+			serviceMap := map[string]string{"name": service, "url": "/" + service + "/v2/api-docs"}
+
+			servicesList = append(servicesList, serviceMap)
+		}
+	}
+
+	resultJson, _ := json.Marshal(servicesList)
+
+	return string(resultJson)
+}
