7b357a79ccaec6114984e84a8184e35253852e76
Nikita Medvedev <nikita.medvedev@leroymerlin.ru>
2019-12-27 20:13:46 +0300

    new logic

--- ingress-autoswagger.go
+++ ingress-autoswagger.go
@@ -6,23 +6,18 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"sort"
+	"strings"
 	"text/template"
 )
 
 func main() {
 	servicesEnv := os.Getenv("SERVICES")
+	//servicesEnv := "app1,app2,app3"
 	if servicesEnv == "" {
 		log.Println("Environment variable \"SERVICES\" is empty")
 		os.Exit(2)
 	}
-
-	services := createServicesJson(servicesEnv)
-	if services == "null" {
-		log.Println("Incorrect variable \"SERVICES\" or no services with swagger. Exit")
-		os.Exit(0)
-	}
-
+	services := strings.Split(servicesEnv, ",")
 	log.Println("Server started on 3000 port!")
 	log.Println(services)
 	html, err := packr.NewBox("./templates").FindString("index.html")
@@ -36,31 +31,36 @@ func main() {
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_ = templateEngine.Execute(w, services)
+		available := Filter(services, func(service string) bool {
+			_, err := http.Get("http://" + service + "/" + service + "/v2/api-docs")
+			return err == nil
+		})
+
+		resultJson, _ := json.Marshal(Map(available, func(service string) interface{} {
+			return map[string]string{
+				"name": service,
+				"url":  "/" + service + "/v2/api-docs",
+			}
+		}))
+		_ = templateEngine.Execute(w, string(resultJson))
 	})
 	_ = http.ListenAndServe(":3000", nil)
 }
 
-func createServicesJson(servicesEnv string) string {
-	var services map[string]map[string]interface{}
-	if err := json.Unmarshal([]byte(servicesEnv), &services); err != nil {
-		panic(err)
-	}
-
-	var servicesList []map[string]string
-	for service, params := range services {
-		if (params["swagger"] != false) && (params["skip"] != true) {
-			log.Println("Generating service: " + service)
-			serviceMap := map[string]string{"name": service, "url": "/" + service + "/v2/api-docs"}
-
-			servicesList = append(servicesList, serviceMap)
+func Filter(vs []string, f func(string) bool) []string {
+	vsf := make([]string, 0)
+	for _, v := range vs {
+		if f(v) {
+			vsf = append(vsf, v)
 		}
 	}
+	return vsf
+}
 
-	sort.SliceStable(servicesList, func(i, j int) bool {
-		return servicesList[i]["name"] < servicesList[j]["name"]
-	})
-	resultJson, _ := json.Marshal(servicesList)
-
-	return string(resultJson)
+func Map(vs []string, f func(string) interface{}) []interface{} {
+	vsm := make([]interface{}, len(vs))
+	for i, v := range vs {
+		vsm[i] = f(v)
+	}
+	return vsm
 }
